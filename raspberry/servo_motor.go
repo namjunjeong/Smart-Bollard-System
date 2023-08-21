ce8745169977c3b967c8a08cd1734ad04214861f
nj_jeong <playjnj@khu.ac.kr>
2023-08-22 00:02:43 +0900

    Modify : servo_motor.go

--- raspberry/servo_motor.go
+++ raspberry/servo_motor.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	"time"
-	"fmt"
+
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
@@ -13,23 +13,22 @@ func main() {
 		log.Fatalf("rpio.Open error : %v", err)
 	}
 	defer rpio.Close()
-	
-	fmt.Println("setting start")
+
 	servo := rpio.Pin(18)
 	servo.Mode(rpio.Pwm)
 	servo.Freq(50) //max freq
 	servo.DutyCycle(0, 200)
-	fmt.Println("setting finish")
 	time.Sleep(time.Second)
-	for i := 0; i<5; i++ {
-		servo.DutyCycle(10,200)
-		time.Sleep(time.Second*2)
-		servo.DutyCycle(16,200)
-		time.Sleep(time.Second*2)
-		servo.DutyCycle(10,200)
-		time.Sleep(time.Second*2)
-		servo.DutyCycle(16,200)
-		time.Sleep(time.Second*2)
-		fmt.Println("end loop")
+	for i := 0; i < 5; i++ {
+		for i := uint32(10); i < 17; i++ {
+			servo.DutyCycle(i, 200)
+			time.Sleep(time.Second / 2)
+		}
+		time.Sleep(time.Second * 2)
+		for i := uint32(16); i > 9; i-- {
+			servo.DutyCycle(i, 200)
+			time.Sleep(time.Second / 2)
+		}
+		time.Sleep(time.Second * 2)
 	}
 }
