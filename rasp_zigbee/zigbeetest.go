ee0b4019af6a9490d21618dcb197c72685c799d5
nj_jeong <playjnj@khu.ac.kr>
2023-08-22 01:12:08 +0900

    Add : zigbeetest.go

---
+++ rasp_zigbee/zigbeetest.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"go.bug.st/serial"
+)
+
+func main() {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+	}
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
+
+	mode := &serial.Mode{
+		BaudRate: 9600,
+	}
+	port, err := serial.Open("/dev/ttyUSB0", mode)
+	if err != nil {
+		log.Fatalf("serial.Open error : %v", err)
+	}
+
+	n, err := port.Write([]byte("10,20,30\n\r"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Sent %v bytes\n", n)
+
+	buff := make([]byte, 100)
+	for {
+		n, err := port.Read(buff)
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+		if n == 0 {
+			fmt.Println("\nEOF")
+			break
+		}
+		fmt.Printf("%v", string(buff[:n]))
+	}
+
+}
