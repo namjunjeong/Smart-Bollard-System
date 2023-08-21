6862c2ca0813200c85024239df56439b414141e4
nj_jeong <playjnj@khu.ac.kr>
2023-08-22 00:26:53 +0900

    Modify : servo_motor.go

--- raspberry/servo_motor.go
+++ raspberry/servo_motor.go
@@ -7,6 +7,24 @@ import (
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
+func BollardInit(pinnum int) rpio.Pin {
+	servo := rpio.Pin(pinnum)
+	servo.Mode(rpio.Pwm)
+	servo.Freq(50) //max freq
+	servo.DutyCycle(0, 200)
+	return servo
+}
+
+func BollardOpen(servo *rpio.Pin, sleeptime time.Duration) {
+	servo.DutyCycle(6, 200)
+	time.Sleep(sleeptime)
+}
+
+func BollardClose(servo *rpio.Pin, sleeptime time.Duration) {
+	servo.DutyCycle(13, 200)
+	time.Sleep(sleeptime)
+}
+
 func main() {
 	err := rpio.Open()
 	if err != nil {
@@ -14,19 +32,11 @@ func main() {
 	}
 	defer rpio.Close()
 
-	servo := rpio.Pin(18)
-	servo.Mode(rpio.Pwm)
-	servo.Freq(50) //max freq
-	servo.DutyCycle(0, 200)
+	servo := BollardInit(18)
+
 	time.Sleep(time.Second)
 	for i := 0; i < 5; i++ {
-		servo.DutyCycle(0,200)
-		time.Sleep(time.Second * 2)
-		servo.DutyCycle(6,200)
-		time.Sleep(time.Second * 2)
-		servo.DutyCycle(13,200)
-		time.Sleep(time.Second * 2)
-		servo.DutyCycle(6,200)
-		time.Sleep(time.Second * 2)
+		BollardClose(&servo, time.Second*2)
+		BollardOpen(&servo, time.Second*2)
 	}
 }
