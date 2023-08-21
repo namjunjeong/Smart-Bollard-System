8b13e9eb5d119bfd1f52f3f410590d06cdd14649
nj_jeong <playjnj@khu.ac.kr>
2023-08-22 02:55:44 +0900

    Modify : stream test files

--- stream/stream_client_thread.py
+++ stream/stream_client_thread.py
@@ -1,32 +1,78 @@
 import cv2
-import asyncio
+import threading
 import os
 from ultralytics import YOLO
 from dotenv import load_dotenv
+from flask import Flask, render_template, Response
 
 load_dotenv()
 img_queue = []
+result_queue = []
+lock = threading.Lock()
 
-async def VideoCollector(cap):
-    ret, frame = cap.read()
-    if len(img_queue) < 4 :
-        img_queue.append(frame)
+class VideoCollector(threading.Thread):
+    def __init__(self, cap):
+        super().__init__()
+        self.cap = cap
 
-async def ObjectDetector(model):
-    if len(img_queue) > 0 :
-        frame = img_queue.pop(0)
-        result = model(frame, verbose=False)[0].plot()
-        cv2.imshow('result', result)
+    def run(self):
+        print("VC thread start")
+        while(True):
+            ret, frame = self.cap.read()
+            lock.acquire()
+            if len(img_queue) < 4 :
+                img_queue.append(frame)
+            lock.release()
 
-async def main():
+class ObjectDetector(threading.Thread):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def run(self):
+        print("OD thread start")
+        while(True):
+            lock.acquire()
+            if len(img_queue) > 0 :
+                frame = img_queue.pop(0)
+                lock.release()
+                result = self.model(frame, verbose=False)[0].plot()
+                result_queue.append(result)
+            else:
+                lock.release()
+
+app = Flask(__name__)
+
+@app.route('/')
+def video_show():
+    return render_template('video_show.html')
+
+def gen_frames():
+    while True:
+        try:
+            ret, buffer = cv2.imencode('.jpg', result_queue.pop(0))
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        except:
+            continue
+
+@app.route('/video')
+def video():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def main():
     model = YOLO(os.getenv("YOLO_MODEL"))
     try:
         cap = cv2.VideoCapture(os.getenv("TCP_VIDEO_URL"))
-        while(True):
-            await VideoCollector(cap)
-            await ObjectDetector(model)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        VC_thread = VideoCollector(cap)
+        VC_thread.setDaemon(True)
+        OD_thread = ObjectDetector(model)
+        OD_thread.setDaemon(True)
+        VC_thread.start()
+        OD_thread.start()
+
+        app.run(host = "0.0.0.0", port = 80)
     except Exception as e:
         print(type(e))
         print(e)
@@ -35,6 +81,4 @@ async def main():
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    asyncio.run(main())
-
-
+    main()
