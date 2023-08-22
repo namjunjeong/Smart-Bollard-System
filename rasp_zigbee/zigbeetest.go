68d68744e4f8460d8d7889cc2eeb0c393a69a835
playjnj <playjnj@khu.ac.kr>
2023-08-23 00:45:52 +0900

    Modify : zigbeetest.go

--- rasp_zigbee/zigbeetest.go
+++ rasp_zigbee/zigbeetest.go
@@ -3,21 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"go.bug.st/serial"
 )
 
 func main() {
-	ports, err := serial.GetPortsList()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(ports) == 0 {
-		log.Fatal("No serial ports found!")
-	}
-	for _, port := range ports {
-		fmt.Printf("Found port: %v\n", port)
-	}
+	var sendmsg string
+	var recvmsg string
+	var bytemsg []byte
+	var wg sync.WaitGroup
+	buff := make([]byte, 100)
 
 	mode := &serial.Mode{
 		BaudRate: 9600,
@@ -26,25 +22,44 @@ func main() {
 	if err != nil {
 		log.Fatalf("serial.Open error : %v", err)
 	}
-
-	n, err := port.Write([]byte("10,20,30\n\r"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Sent %v bytes\n", n)
-
-	buff := make([]byte, 100)
-	for {
-		n, err := port.Read(buff)
-		if err != nil {
-			log.Fatal(err)
-			break
+	
+	wg.Add(2)
+	go func(){
+		defer wg.Done()
+		for{
+			fmt.Scanln(&sendmsg)
+			bytemsg = []byte(sendmsg)
+			_, err := port.Write(bytemsg)
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+			if sendmsg == "quit"{
+				break
+			}
 		}
-		if n == 0 {
-			fmt.Println("\nEOF")
-			break
-		}
-		fmt.Printf("%v", string(buff[:n]))
-	}
+		fmt.Println("sender done")
+	}()
 
+	go func(){
+		defer wg.Done()
+		for{
+			n,err:=port.Read(buff)
+			if err != nil{
+				log.Fatal(err)
+				break
+			}
+			if n==0{
+				fmt.Println("\nEOF")
+				break
+			}
+			recvmsg = string(buff[:n])
+			if recvmsg == "quit"{
+				break
+			}
+			fmt.Println(recvmsg)
+		}
+		fmt.Println("receiver done")
+	}()
+	wg.Wait()
 }
